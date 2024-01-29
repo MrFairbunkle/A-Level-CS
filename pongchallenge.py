a479d2557098307c7384ab0f20167c297494e0af
MrFairbunkle <charliebhfw@icloud.com>
2024-01-29 10:25:13 +0000

    hgfyjgk

--- pongchallenge.py
+++ pongchallenge.py
@@ -1,4 +1,7 @@
+# imports
 import pygame, random, sys
+
+# general code
 pygame.display.set_caption("BadPong")
 clock=pygame.time.Clock()
 screen_width=1000
@@ -6,18 +9,26 @@ screen_height=500
 
 pygame.init()
 
+# variables
 screen=pygame.display.set_mode((screen_width,screen_height))
+timepassed=0
 redsize=15
 bluesize=15
+ballwidth=7
+ballradius=7
 redsquare=pygame.Rect(920,200,redsize,redsize*6)
 bluesquare=pygame.Rect(80,200,bluesize,bluesize*6)
+ball=pygame.Rect(screen_height//2,screen_width//2,ballwidth,ballradius)
 redsquare.y
 bluesquare.y
+ball.y
+ball.x
 pygame.display.flip()
 RED=(255,0,0)
 BLUE=(0,0,255)
 BLACK=(0,0,0)
 
+# main loop
 running=True
 while running:
     for event in pygame.event.get():
@@ -38,10 +49,13 @@ while running:
         if redsquare.y + 5 <= screen_height - 80:
             redsquare.y+=5
 
+    while ball.x<=0:
+        ball.x+=5
+
     screen.fill("#ffffff")
     pygame.draw.rect(screen,RED,redsquare)
     pygame.draw.rect(screen,BLUE,bluesquare)
-    pygame.draw.circle(screen,(BLACK),(screen_width//2,screen_height//2),7,7)
+    pygame.draw.circle(screen,(BLACK),(screen_width//2,screen_height//2),ballwidth,ballradius)
 
     pygame.display.update()
     clock.tick(60)
