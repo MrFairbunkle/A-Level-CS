b448043bf73d9abc8a6954189717757ce5895db2
mrfairbunkle <charliebhfw@icloud.com>
2024-03-15 12:46:45 +0000

    fixed

--- flappybird.py
+++ flappybird.py
@@ -1,7 +1,5 @@
-# Imports
 import pygame, random, time, sys
 
-# General Code
 pygame.init()
 pygame.display.set_caption("FlappyBird")
 clock = pygame.time.Clock()
@@ -9,7 +7,6 @@ screen_width = 1000
 screen_height = 500
 font = pygame.font.SysFont('Bauhaus 93', 20)
 
-# Variables
 screen = pygame.display.set_mode((screen_width, screen_height))
 score = 0
 gametime = 0
@@ -18,7 +15,6 @@ birdradius = 7
 pillargap = 200
 pillarwidth = 35
 pillar_speed_x = -5
-bird = pygame.image.load("bird.jfif")
 bird_speed_y = 0
 gravity = 0.5
 last_score_update = time.time()
@@ -28,18 +24,18 @@ GREEN = (53, 129, 34)
 BLACK = (0, 0, 0)
 WHITE = (230, 230, 230)
 
-# Pillar Generation
 def generate_pillars():
     pillarheight = random.randint(50, screen_height - pillargap - 50)
     bottompillar = pygame.Rect(screen_width, 0, pillarwidth, pillarheight)
-    toppillar = pygame.Rect(screen_width  , pillarheight + pillargap, pillarwidth, screen_height - pillarheight - pillargap)
+    toppillar = pygame.Rect(screen_width, pillarheight + pillargap, pillarwidth, screen_height - pillarheight - pillargap)
     return bottompillar, toppillar
 
 bottompillar, toppillar = generate_pillars()
 
-bird = pygame.Rect(300, screen_height // 2, birdwidth, birdradius)
+birdy = pygame.image.load("bird.jfif")
+bird = pygame.transform.scale(birdy, (50, 50))
+bird_rect = bird.get_rect(topleft=(100, screen_height // 2))
 
-# Main Loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -51,7 +47,7 @@ while running:
         bird_speed_y = -7
 
     bird_speed_y += gravity
-    bird.y += bird_speed_y
+    bird_rect.y += bird_speed_y
 
     bottompillar.x += pillar_speed_x
     toppillar.x += pillar_speed_x
@@ -59,10 +55,10 @@ while running:
     if bottompillar.right < 0:
         bottompillar, toppillar = generate_pillars()
 
-    if bird.top <= 0 or bird.bottom >= screen_height:
+    if bird_rect.top <= 0 or bird_rect.bottom >= screen_height:
         running = False
 
-    if bird.colliderect(bottompillar) or bird.colliderect(toppillar):
+    if bird_rect.colliderect(bottompillar) or bird_rect.colliderect(toppillar):
         running = False
 
     current_time = time.time()
@@ -74,12 +70,12 @@ while running:
 
     pygame.draw.rect(screen, GREEN, toppillar)
     pygame.draw.rect(screen, GREEN, bottompillar)
-    pygame.draw.circle(screen, BLACK, (bird.x, bird.y), birdwidth, birdradius)
+    screen.blit(bird, bird_rect.topleft)
 
     score_text = font.render("Score: " + str(score), True, BLACK)
     screen.blit(score_text, (10, 10))
 
-    pygame.display.flip()
+    pygame.display.update()
 
     clock.tick(60)
 
